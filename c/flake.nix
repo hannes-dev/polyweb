c21d8e79c439bb682dc57b4fe9a0b73aadbec2f9
Hannes <hannes-dev@mailbox.org>
2025-05-27 03:14:57 +0200

    start c

---
+++ c/flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "C webserver";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "c-web-server";
+          version = "0.1.0";
+          src = "./.";
+
+          nativeBuildInputs = [
+            pkgs.clang
+          ];
+
+          buildPhase = ''
+            clang src/hello.c -o c-web-server
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp c-web-server $out/bin/
+          '';
+        };
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.clang        
+            pkgs.gnumake       
+            pkgs.gdb        
+            pkgs.clang-tools
+            pkgs.valgrind
+          ];
+        };
+      }
+    );
+}
---
+++ c/src/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // For printf
+
+int main(int argc, char *argv[]) {
+    printf("Hello, C Project!\n");
+    return 0;
+}
+
